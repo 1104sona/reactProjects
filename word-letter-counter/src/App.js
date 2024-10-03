aba709f438c28005905c5c79140aa69336b58860
1104sona panda <sonali.pnd04@gmail.com>
2024-10-03 05:02:21 +0000

    word-letter-counter successful

---
+++ word-letter-counter/src/App.js
@@ -0,0 +1,18 @@
+import WordLetterCount from './WordLetterCounter.js';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 id="top"> 
+            Mini project 1
+            </h1> 
+            <h1> 
+            Words and Letters 
+            Counter 
+            </h1> 
+            <WordLetterCount/> 
+    </div>
+  );
+}
+
+export default App ;
